de288c73cfd6399427fd58fed3d9914c3535b76b
hamerski-n <58872340+hamerski-n@users.noreply.github.com>
2020-04-11 21:23:46 -0700

    L6.0

---
+++ api/routes/products.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+
+const Product = require('../models/product');
+const mongoose = require('mongoose');
+
+router.get('/', (req,res,next)=>{
+    res.status(200).json({
+       message: 'Handling GET requests to /products'
+    });
+});
+
+router.post('/', (req,res,next)=>{
+    // const product = {
+    //     name: req.body.name,
+    //     price: req.body.price
+    // };
+    const product = new Product({
+       _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    });
+    product
+        .save()
+        .then(result => {
+            console.log(result)
+        })
+        .catch(err => console.log(err));
+    res.status(201).json({
+        message: 'Handling POST requests to /products',
+        createProduct: product
+    });
+});
+
+router.get('/:productId', (req,res,next)=>{
+    const id = req.params.productId;
+    if (id==='special'){
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        });
+    }else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:productId', (req,res,next)=>{
+        res.status(200).json({
+            message: 'Updated product!'
+        });
+});
+
+router.delete('/:productId', (req,res,next)=>{
+    res.status(200).json({
+        message: 'Deleted product'
+    });
+});
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,60 @@
+const  express = require('express');  //require package express
+const  app =  express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect(
+    'mongodb+srv://admin:'+
+    process.env.MONGO_ATLAS_PW +
+    '@cluster0-mazuv.mongodb.net/test?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+        // useMongoClient: true
+    }
+);
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json()); //will extract json data
+
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', '*'); //*- we accept requests from any website
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    ); //*- we accept requests with '..'headers
+
+    if (req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+
+//Routes which should handle requests
+app.use('/products',productRoutes);
+app.use('/orders',orderRoutes);
+
+app.use((req,res,next)=>{
+   const error = new Error('Not found');
+   error.status = 404;
+   next(error);
+});
+
+app.use((error,req,res,next)=>{
+   res.status(error.status || 500);
+   res.json({
+       error:{
+           message: error.message
+       }
+   });
+});
+
+module.exports = app;
