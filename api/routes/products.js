afb36b45c94159efd272c111514d721bd86686c6
hamerski-n <58872340+hamerski-n@users.noreply.github.com>
2020-04-11 23:50:22 -0700

    L6.2

--- api/routes/products.js
+++ api/routes/products.js
@@ -88,16 +88,16 @@ router.patch("/:productId", (req, res, next) => {
         });
 });
 
-router.delete("/:productId", (req, res, next) => {
+router.delete("/:productId", (req, response, next) => {
     const id = req.params.productId;
     Product.remove({ _id: id })
         .exec()
         .then(result => {
-            res.status(200).json(result);
+            response.status(200).json(result);
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json({
+            response.status(500).json({
                 error: err
             });
         });
