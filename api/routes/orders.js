615667ebf6b6bb8960e29f9ef806f70a1e94a1bd
hamerski-n <58872340+hamerski-n@users.noreply.github.com>
2020-04-11 23:20:17 -0700

    L6.1

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,31 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res,next)=>{
-            res.status(200).json({
-                message: 'Orders were fetched'
+// Handle incoming GET requests to /orders
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Orders were fetched'
     });
 });
 
-router.post('/', (req,res,next)=>{
+router.post('/', (req, res, next) => {
     const order = {
         productId: req.body.productId,
         quantity: req.body.quantity
     };
     res.status(201).json({
-        message: 'Orders was created',
+        message: 'Order was created',
         order: order
     });
 });
 
-router.get('/:orderId', (req,res,next)=>{
+router.get('/:orderId', (req, res, next) => {
     res.status(200).json({
         message: 'Order details',
         orderId: req.params.orderId
     });
 });
 
-router.delete('/:orderId', (req,res,next)=>{
+router.delete('/:orderId', (req, res, next) => {
     res.status(200).json({
         message: 'Order deleted',
         orderId: req.params.orderId
