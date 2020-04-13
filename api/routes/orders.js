7a9541ac59552264f7a53e87a617410d0a61d887
hamerski-n <58872340+hamerski-n@users.noreply.github.com>
2020-04-12 18:54:09 -0700

    L8 - Managing orders with Mongoose. Orders GET and POST

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,22 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
+const Order = require('../models/order');
 // Handle incoming GET requests to /orders
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Orders were fetched'
-    });
+    Order.find()
+        .exec()
+        .then(docs=>{
+           res.status(200).json(docs)
+        })
+        .catch(err=>{
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.post('/', (req, res, next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    };
-    res.status(201).json({
-        message: 'Order was created',
-        order: order
+    const order = new Order({
+        _id: mongoose.Types.ObjectId(),
+        product: req.body.productId,
+        quantity: req.body.quantity,
     });
+    order
+        .save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+
 });
 
 router.get('/:orderId', (req, res, next) => {
