00593e34699ae1006ad74d038d9ccd11386cef5d
hamerski-n <58872340+hamerski-n@users.noreply.github.com>
2020-04-13 17:45:43 -0700

    L12 User login add a token

--- api/models/user.js
+++ api/models/user.js
@@ -3,7 +3,12 @@ const  mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
    _id: mongoose.Schema.Types.ObjectId,
    // email: {type: mongoose.Schema.Types.ObjectId, ref: 'Product',required: true},
-   email: {type: String, required: true},
+   email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+   },
    password: {type: String, required: true}
 });
 
